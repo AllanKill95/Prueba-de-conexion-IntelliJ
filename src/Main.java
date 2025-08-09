1e31afb2fa3f15bd1700755d5c91e23a4c515eae
AllanKill95 <allan.sanchez3095@unitec.edu>
2025-08-08 19:07:27 -0600

    Hola Mundo

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         for (int i = 1; i <= 5; i++) {
         
-            System.out.println("i = " + i);
+            System.out.printf("This is message number %d%n", i);
         }
     }
 }
